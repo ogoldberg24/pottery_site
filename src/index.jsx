7a758853c13b73dee524236184dde87839bc3984
pk8189 <pmkelly4444@gmail.com>
2019-04-29 21:52:48 -0400

    work with react scripts instead of webpack

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ const render = (Component) => {
         <Component/>
       </HashRouter>
     </AppContainer>,
-    document.getElementById('react-app-root')
+    document.getElementById('root')
   );
 };
 
