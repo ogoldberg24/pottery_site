84601b5748ac471040a4c7b16f900a1e4f0baf6e
patrickmkelly <patrick.kelly@eigentech.com>
2019-06-01 17:40:44 -0400

    start on flickr

--- src/components/pages/Dining.jsx
+++ src/components/pages/Dining.jsx
@@ -3,114 +3,40 @@ import Nav from '../ui/Nav';
 import Header from '../ui/Header';
 import Gallery from 'react-grid-gallery';
 
-import bowl1 from '../../assets/pottery_grid_images/bowl1.jpg';
-import bowl2 from '../../assets/pottery_grid_images/bowl2.jpg'
-import bowl3 from '../../assets/pottery_grid_images/bowl3.jpg'
-import bowl4 from '../../assets/pottery_grid_images/bowl4.jpg'
-import bowl5 from '../../assets/pottery_grid_images/bowl5.jpg'
-import bowl6 from '../../assets/pottery_grid_images/bowl6.jpg'
-import bowl7 from '../../assets/pottery_grid_images/bowl7.jpg'
-import bowl8 from '../../assets/pottery_grid_images/bowl8.jpg'
-import bowl9 from '../../assets/pottery_grid_images/bowl9.jpg'
-import plate1 from '../../assets/pottery_grid_images/plate1.jpg'
-import plate2 from '../../assets/pottery_grid_images/plate2.jpg'
 
+import foodBanner from '../../assets/images/food.jpg';
+ 
+class Dining extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      photoIds: [],
+    };
+  }
 
-import careersBanner from '../../assets/images/food.jpg';
+  componentDidMount() {
+    fetch(
+      "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=91325757bece30c1b07fa0be1f91dd32&photoset_id=72157708875167616&user_id=181769962@N03&format=json&nojsoncallback=1",
+    )
+    .then(response => response.json())
+    .then(data => data.photoset.photo.map(photoObj => [photoObj.id, photoObj.secret]))
+    .then(data => this.setState({photoIds: data}))
+  }
 
-
-
-function Dining() {
-
-  const dining = [
-    {
-      "src": bowl1,
-      "thumbnail": bowl1,
-      "thumbnailWidth": '1000',
-      "thumbnailHeight": '1000',
-    },
-    {
-      "src": bowl2,
-      "thumbnail": bowl2,
-      "thumbnailWidth": '1000',
-      "thumbnailHeight": '1000',
-    },
-    {
-      "src": bowl3,
-      "thumbnail": bowl3,
-      "thumbnailWidth": '1000',
-      "thumbnailHeight": '1000',
-    },
-    {
-      "src": bowl4,
-      "thumbnail": bowl4,
-      "thumbnailWidth": '1000',
-      "thumbnailHeight": '1000',
-    },
-    {
-      "src": bowl5,
-      "thumbnail": bowl5,
-      "thumbnailWidth": '1000',
-      "thumbnailHeight": '1000',
-    },
-    {
-      "src": bowl6,
-      "thumbnail": bowl6,
-      "thumbnailWidth": '1000',
-      "thumbnailHeight": '1000',
-    },
-    {
-      "src": bowl7,
-      "thumbnail": bowl7,
-      "thumbnailWidth": '1000',
-      "thumbnailHeight": '1000',
-    },
-    {
-      "src": bowl8,
-      "thumbnail": bowl8,
-      "thumbnailWidth": '1000',
-      "thumbnailHeight": '1000',
-    },
-    {
-        "src": bowl9,
-        "thumbnail": bowl9,
-        "thumbnailWidth": '1000',
-        "thumbnailHeight": '1000',
-    },
-    {
-        "src": plate1,
-        "thumbnail": plate1,
-        "thumbnailWidth": '1000',
-        "thumbnailHeight": '1000',
-    },
-    {
-        "src": plate2,
-        "thumbnail": plate2,
-        "thumbnailWidth": '1000',
-        "thumbnailHeight": '1000',
-    },
-  ]
-
-  const divStyle = {
-    justifyContent: 'center',
-    width: '95%'
-    
-  };
-
-  return (
-    <div>
-      <Header bgImg={careersBanner}
-        head="For dining"
-        pageTitle="CUPS AND BOWLS"/>
-      <Nav />
-      <center>
-      <div style={divStyle}>
-        <Gallery images={dining} enableImageSelection={false}/>
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        <Header bgImg={foodBanner}
+          head="For dining"
+          pageTitle="CUPS AND BOWLS"/>
+        <Nav />
+        <center>
+        </center>
       </div>
-      </center>
-    </div>
-  );
+    );
+  }
 }
 
 export default Dining;
